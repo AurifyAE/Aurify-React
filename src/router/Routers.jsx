9a7cd6883aa89debe378db7bf08f85fe02be7e96
ABDUL MAJID K <majidmaji3410@gmail.com>
2024-08-24 10:52:58 +0530

    handle the protect router and jwt

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -1,31 +1,37 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Login from '../components/login/Login';
-import DashboardLayout from '../layout/DashboardLayout';
-import SpotRateLayout from '../layout/SpotRateLayout';
-import MediaLayout from '../layout/MediaLayout';
-import MessagesLayout from '../layout/MessagesLayout';
-import NewsLayout from '../layout/NewsLayout';
-import ShopLayout from '../layout/ShopLayout';
-import ProfileLayout from '../layout/ProfileLayout';
-import BankDetailsLayout from '../layout/BankDetailsLayout';
-import UsersLayout from '../layout/UsersLayout';
-import NotFound from '../pages/NotFound';
-import ServerError from '../pages/ServerError';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "../components/login/Login";
+import DashboardLayout from "../layout/DashboardLayout";
+import SpotRateLayout from "../layout/SpotRateLayout";
+import MediaLayout from "../layout/MediaLayout";
+import MessagesLayout from "../layout/MessagesLayout";
+import NewsLayout from "../layout/NewsLayout";
+import ShopLayout from "../layout/ShopLayout";
+import ProfileLayout from "../layout/ProfileLayout";
+import BankDetailsLayout from "../layout/BankDetailsLayout";
+import UsersLayout from "../layout/UsersLayout";
+import MarketClosingayout from "../layout/MarketClosingLayout";
+import NotFound from "../pages/NotFound";
+import ServerError from "../pages/ServerError";
+import Protect from "../protectorRouter/adminProtect";
 
 function Routers() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="dashboard" element={<DashboardLayout />} />
-      <Route path="spot-rate" element={<SpotRateLayout />} />
-      <Route path="media" element={<MediaLayout />} />
-      <Route path="messages" element={<MessagesLayout />} />
-      <Route path="news" element={<NewsLayout />} />
-      <Route path="shop" element={<ShopLayout />} />
-      <Route path="users" element={<UsersLayout />} />
-      <Route path="profile" element={<ProfileLayout />} />
-      <Route path="bank-details" element={<BankDetailsLayout />} />
+      <Route element={<Protect/>}>
+        <Route path="dashboard" element={<DashboardLayout />} />
+        <Route path="spot-rate" element={<SpotRateLayout />} />
+        <Route path="media" element={<MediaLayout />} />
+        <Route path="messages" element={<MessagesLayout />} />
+        <Route path="news" element={<NewsLayout />} />
+        <Route path="shop" element={<ShopLayout />} />
+        <Route path="users" element={<UsersLayout />} />
+        <Route path="profile" element={<ProfileLayout />} />
+        <Route path="bank-details" element={<BankDetailsLayout />} />
+        <Route path="market-closing" element={<MarketClosingayout />} />
+      </Route>
+
       <Route path="*" element={<NotFound />} />
       <Route path="500" element={<ServerError />} />
     </Routes>
---
+++ src/utils/firebaseUtils.js
@@ -0,0 +1,39 @@
+// Import Firebase and FCM types
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, Messaging } from 'firebase/messaging';
+
+// Define the Firebase configuration object with appropriate types
+const firebaseConfig = {
+  apiKey: 'AIzaSyCAHapnxRWqH8lwgYuFjrHQ7rDrkRlSe54',
+  authDomain: 'pushnotifaction-11aab.firebaseapp.com',
+  projectId: 'pushnotifaction-11aab',
+  storageBucket: 'pushnotifaction-11aab.appspot.com',
+  messagingSenderId: '329227476273',
+  appId: '1:329227476273:web:d977025db3317cafeee410',
+  measurementId: 'G-LB4BB5PSG5',
+};
+
+// Define the VAPID key as a constant string
+const vapidKey = 'BEugoszFFht1wxmeOdaA_3f3mpT_x-gR3KneVOa2zUcl3jPOBVmwqSoNncQyPQcgdggcT7C1yTXtx5mn8bbJcVY';
+
+// Initialize Firebase app
+const app = initializeApp(firebaseConfig);
+
+// Get messaging instance from Firebase
+const messaging = getMessaging(app);
+
+// Function to request FCM token
+export const requestFCMToken = async () => {
+  try {
+    const permission = await Notification.requestPermission();
+    if (permission === 'granted') {
+      // Get the FCM token using the VAPID key
+      return getToken(messaging, { vapidKey });
+    } else {
+      throw new Error('Notification permission not granted');
+    }
+  } catch (error) {
+    console.error('Error getting FCM token:', error);
+    throw error;
+  }
+};
