f2412e52855a57fde38d92def78c8ecda5086f74
Amaljith M K <amaljithmk@Amaljiths-MacBook-Air.local>
2026-05-07 16:36:22 +0530

    changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-100 text-gray-400 py-4 text-sm">
       <div className="container mx-4 flex justify-between items-center">
-        <p>© 2024, made   by Aurify Solutions</p>
+        <p>© 2024, made  by Aurify Technologies</p>
         <div className="flex space-x-4 mr-8">
           <p><a href="https://www.tecnavis.com" className="hover:text-gray-600">Tecnavis</a></p>
           <p><a href="https://www.tecnavis.com/about" className="hover:text-gray-600">About Us</a></p>
